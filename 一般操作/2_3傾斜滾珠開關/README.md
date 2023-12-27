341b11703e3ffd9a0424e5f3ab805f90f05f56c7
roberthsu2003 <roberthsu2003@gmail.com>
2023-12-27 13:19:44 +0800

    modify

--- 一般操作/2_3傾斜滾珠開關/README.md
+++ 一般操作/2_3傾斜滾珠開關/README.md
@@ -29,9 +29,28 @@
 ![](./images/pic4.jpeg)
 
 
-### 線路圖
+### 線路圖(使用上接電阻)
 
 ![](./images/pic5.png)
 
 
+### 範例
+
+```
+from machine import Pin
+from machine import Timer
+
+
+led = Pin(15,Pin.OUT)
+tilt_switch = Pin(16, Pin.IN, Pin.PULL_UP) # 上接電阻
+
+def check_tilt(t:Timer):
+    tilt_value = tilt_switch.value()
+    print(tilt_value)
+    led.value(tilt_value)
+    
+
+time1 = Timer() 
+time1.init(period=1000, mode=Timer.PERIODIC, callback=check_tilt)
+```
 
--- 一般操作/2_3傾斜滾珠開關/main.py
+++ 一般操作/2_3傾斜滾珠開關/main.py
@@ -1,29 +1,17 @@
 from machine import Pin
 from machine import Timer
-import time
+
 
 led = Pin(15,Pin.OUT)
-sr501 = Pin(2,Pin.IN)
+tilt_switch = Pin(16, Pin.IN, Pin.PULL_UP) # 上接電阻
 
-sensors:list[int] = [] #收集5秒資訊
-def check_people(t:Timer):
-    global sensors
-    sensor_state = sr501.value()
-    print(sensor_state)
-    length:int = len(sensors)
-    if length >= 5: #大於=於5筆       
-        if sum(sensors) > 0: #大於1,代表有人
-            led.value(1)
-            print("總合大於1")
-        else:
-            led.value(0)
-        
-        sensors = [] #清空重新計算
-    else:        
-        sensors.append(sensor_state)
+def check_tilt(t:Timer):
+    tilt_value = tilt_switch.value()
+    print(tilt_value)
+    led.value(tilt_value)
     
 
 time1 = Timer() 
-time1.init(period=1000, mode=Timer.PERIODIC, callback=check_people)
+time1.init(period=1000, mode=Timer.PERIODIC, callback=check_tilt)
 
 
